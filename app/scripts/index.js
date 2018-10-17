3801f684a754dda32b1106b6587b50b581b0e9ba
Andrew Wanex <awanex2705@gmail.com>
2018-10-17 11:08:25 -0700

    feat: create main page script

---
+++ app/scripts/index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', event => {
+  const defaultLocation = {
+    latitude: 34.0522,
+    longitude: -118.2437
+  };
+  const locationService = new LocationService(defaultLocation.latitude, defaultLocation.longitude);
+  const weatherService = new WeatherService();
+  const geoService = new GeoService();
+  const spaceService = new SpaceService();
+
+  const homeView = new HomeView(
+    document,
+    locationService,
+    weatherService,
+    geoService,
+    spaceService
+  );
+  homeView.loadContent();
+});
