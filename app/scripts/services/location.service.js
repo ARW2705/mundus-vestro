191fd51910181751672e8548df962a5bf4e29499
Andrew Wanex <awanex2705@gmail.com>
2018-10-31 10:26:21 -0700

    feat: add geocode and reverse geocode methods

--- app/scripts/services/location.service.js
+++ app/scripts/services/location.service.js
@@ -5,11 +5,41 @@
 **/
 class LocationService {
   // Set default location
-  constructor(latitude, longitude) {
+  constructor(_document, latitude, longitude) {
+    this._document = _document;
     this.latitude = latitude;
     this.longitude = longitude;
     this.eastBoundary = longitude + 180;
     this.westBoundary = longitude - 180;
+  }
+
+  /**
+   * GIS API base URL
+   *
+   * params: none
+   *
+   * return: string
+   * - arcgis base url
+  **/
+  static get GIS_API_BASE_URL() {
+    return 'http://geocode.arcgis.com/arcgis/rest/services/World';
+  }
+
+  /**
+   * Create the complete URL with given query parameters for GIS API
+   *
+   * params: object
+   * query - contains gis route and search params
+   *
+   * return: object
+   * - new URL with search parameters
+  **/
+  createGISCompleteURL(query) {
+    const url = new URL(LocationService.GIS_API_BASE_URL + query.route);
+    for (const key in query.params) {
+      url.searchParams.append(key, query.params[key]);
+    }
+    return url;
   }
 
   /**
@@ -60,6 +90,7 @@ class LocationService {
           this.longitude = position.coords.longitude;
           this.eastBoundary = position.coords.longitude + 180;
           this.westBoundary = position.coords.longitude - 180;
+          this.postLocationUpdate();
         },
         error => {
           console.log(`Error utilizing geolocation ${error.status}: ${error.message}`);
@@ -70,9 +101,105 @@ class LocationService {
       this.longitude = longitude;
       this.eastBoundary = longitude + 180;
       this.westBoundary = longitude - 180;
+      this.postLocationUpdate();
     } else {
       console.log('Geolocation not available');
     }
+  }
+
+  /**
+   * Post message that location has been updated
+   *
+   * params: none
+   *
+   * return: none
+  **/
+  postLocationUpdate() {
+    const newEvent = new CustomEvent('location-update', {
+      detail: {
+        latitude: this.latitude,
+        longitude: this.longitude
+      }
+    });
+    this._document.dispatchEvent(newEvent);
+  }
+
+  /**
+   * Get latitude and longitude by city or zipcode
+   *
+   * params: string
+   * locationData - location information (city and/or zipcode)
+   *
+   * return: object
+   * - Promise that resolves with new latitude and longitude coordinates
+  **/
+  getGeocode(locationData) {
+    const query = {
+      route: '/GeocodeServer/findAddressCandidates',
+      params: {
+        f: 'pjson',
+        singleLine: locationData
+      }
+    };
+    return fetch(this.createGISCompleteURL(query))
+      .then(res => {
+        return res.json()
+          .then(address => {
+            if (address.candidates.length) {
+              const lat = parseFloat(address.candidates[0].location.y.toFixed(4));
+              const long = parseFloat(address.candidates[0].location.x.toFixed(4));
+              this.setLocation(null, lat, long);
+              return Promise.resolve({
+                latitude: lat,
+                longitude: long
+              });
+            } else {
+              return Promise.reject('Missing address match');
+            }
+          })
+          .catch(error => {
+            return Promise.reject(error);
+          })
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
+  }
+
+  /**
+   * Use reverse geocoding to get the nearest city name and zip code
+   *
+   * params: object
+   * latlng - latitude and longitude to be searched
+   *
+   * return: object
+   * - Promise that resolve with city name and zip code
+  **/
+  getReverseGeocode(latlng) {
+    const query = {
+      route: '/GeocodeServer/reverseGeocode',
+      params: {
+        f: 'pjson',
+        featureTypes: 'postal',
+        location: `${latlng.longitude},${latlng.latitude}`
+      }
+    };
+    return fetch(this.createGISCompleteURL(query))
+      .then(res => {
+        return res.json()
+          .then(geocode => {
+            return Promise.resolve({
+              city: geocode.address.City,
+              zipcode: geocode.address.Postal
+            });
+          })
+          .catch(error => {
+            return Promise.reject(error);
+          })
+      })
+      .catch(error => {
+        return Promise.reject(error);
+      });
   }
 
 }
