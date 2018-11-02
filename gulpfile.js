93a85ac4e3df5837396331c1059be8d24e571ac2
Andrew Wanex <awanex2705@gmail.com>
2018-11-01 18:18:01 -0700

    feat: separate dev preview tasks and build tasks

--- gulpfile.js
+++ gulpfile.js
@@ -9,32 +9,40 @@ const babel = require('gulp-babel');
 const browserSync = require('browser-sync').create();
 const imageResize = require('gulp-image-resize');
 const imagemin = require('gulp-imagemin');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
 
 const paths = {
   html: {
     src: 'app/*.html',
-    dest: 'dist'
+    buildDest: 'dist',
+    previewDest: 'tmp'
   },
   fonts: {
     src: 'app/assets/fonts/**/*',
-    dest: 'dist/assets/fonts'
+    buildDest: 'dist/assets/fonts',
+    previewDest: 'tmp/assets/fonts'
   },
   icons: {
     src: 'app/assets/icons/**/*',
-    dest: 'dist/assets/icons'
+    buildDest: 'dist/assets/icons',
+    previewDest: 'tmp/assets/icons'
   },
   images: {
     src: 'app/assets/images/**/*',
-    dest: 'dist/assets/images'
+    buildDest: 'dist/assets/images',
+    previewDest: 'tmp/assets/images'
   },
   scss: {
     src: 'app/styles/*.scss',
     watch: 'app/styles/**/*.scss',
-    dest: 'dist/styles'
+    buildDest: 'dist/styles',
+    previewDest: 'tmp/styles'
   },
   wicons: {
     src: 'app/styles/vendor/weather-icons/*.css',
-    dest: 'dist/styles'
+    buildDest: 'dist/styles',
+    previewDest: 'tmp/styles'
   },
   scripts: {
     srcShared: [
@@ -75,12 +83,16 @@ const paths = {
     srcLoader: 'app/scripts/shared/loader.js',
     sw: {
       src: 'app/sw.js',
-      dest: 'dist'
+      manifest: 'app/manifest.json',
+      buildDest: 'dist',
+      previewDest: 'tmp'
     },
-    dest: 'dist/scripts'
+    buildDest: 'dist/scripts',
+    previewDest: 'tmp/scripts'
   },
   clean: {
-    src: 'dist'
+    buildSrc: 'dist',
+    previewSrc: 'tmp'
   },
 };
 
@@ -101,23 +113,23 @@ const previewTasks = [
 ];
 
 gulp.task('clean', () => {
-  return gulp.src(paths.clean.src, {read: false})
+  return gulp.src(paths.clean.previewSrc, {read: false})
     .pipe(clean())
 });
 
 gulp.task('html', () => {
   return gulp.src(paths.html.src)
-    .pipe(gulp.dest(paths.html.dest))
+    .pipe(gulp.dest(paths.html.previewDest))
 });
 
 gulp.task('fonts', () => {
   return gulp.src(paths.fonts.src)
-    .pipe(gulp.dest(paths.fonts.dest))
+    .pipe(gulp.dest(paths.fonts.previewDest))
 });
 
 gulp.task('icons', () => {
   return gulp.src(paths.icons.src)
-    .pipe(gulp.dest(paths.icons.dest))
+    .pipe(gulp.dest(paths.icons.previewDest))
 });
 
 gulp.task('images', () => {
@@ -127,21 +139,20 @@ gulp.task('images', () => {
       imageMagick: true
     }))
     .pipe(imagemin({progressive: true}))
-    .pipe(gulp.dest(paths.images.dest))
+    .pipe(gulp.dest(paths.images.previewDest))
 });
 
 gulp.task('sass', () => {
   return gulp.src(paths.scss.src)
-    // .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
     .pipe(sass()).on('error', sass.logError)
-    .pipe(gulp.dest(paths.scss.dest))
+    .pipe(gulp.dest(paths.scss.previewDest))
     .pipe(browserSync.stream())
 });
 
 gulp.task('wicons', () => {
   return gulp.src(paths.wicons.src)
     .pipe(concat('wicons.css'))
-    .pipe(gulp.dest(paths.wicons.dest))
+    .pipe(gulp.dest(paths.wicons.previewDest))
 });
 
 gulp.task('shared-scripts', () => {
@@ -150,7 +161,7 @@ gulp.task('shared-scripts', () => {
       presets: ['@babel/env']
     }))
     .pipe(concat('shared.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.previewDest))
 });
 
 gulp.task('home-view', () => {
@@ -159,7 +170,7 @@ gulp.task('home-view', () => {
       presets: ['@babel/env']
     }))
     .pipe(concat('index.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.previewDest))
 });
 
 gulp.task('sky-view', () => {
@@ -168,7 +179,7 @@ gulp.task('sky-view', () => {
       presets: ['@babel/env']
     }))
     .pipe(concat('sky.details.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.previewDest))
 });
 
 gulp.task('earth-view', () => {
@@ -177,7 +188,7 @@ gulp.task('earth-view', () => {
       presets: ['@babel/env']
     }))
     .pipe(concat('earth.details.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.previewDest))
 });
 
 gulp.task('space-view', () => {
@@ -186,7 +197,7 @@ gulp.task('space-view', () => {
       presets: ['@babel/env']
     }))
     .pipe(concat('space.details.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.previewDest))
 });
 
 gulp.task('loader', () => {
@@ -195,7 +206,7 @@ gulp.task('loader', () => {
       presets: ['@babel/env']
     }))
     .pipe(concat('loader.min.js'))
-    .pipe(gulp.dest(paths.scripts.dest))
+    .pipe(gulp.dest(paths.scripts.previewDest))
 });
 
 gulp.task('sw', () => {
@@ -203,7 +214,7 @@ gulp.task('sw', () => {
     .pipe(babel({
       presets: ['@babel/env']
     }))
-    .pipe(gulp.dest(paths.scripts.sw.dest))
+    .pipe(gulp.dest(paths.scripts.sw.previewDest))
 });
 
 gulp.task('preview', previewTasks, () => {
@@ -213,5 +224,152 @@ gulp.task('preview', previewTasks, () => {
 
   gulp.watch(paths.scss.watch, ['sass']);
 });
+
+/* === END PREVIEW TASKS === */
+
+/*-----------------------------------------------------------*/
+
+/* === BUILD TASKS === */
+
+const buildTasks = [
+  'build-shared-scripts',
+  'build-home-view',
+  'build-sky-view',
+  'build-earth-view',
+  'build-space-view',
+  'build-loader',
+  'build-sw',
+  'build-html',
+  'build-fonts',
+  'build-icons',
+  'build-images',
+  'build-wicons',
+  'build-sass',
+  'build-manifest'
+];
+
+gulp.task('build-clean', () => {
+  return gulp.src(paths.clean.buildSrc, {read: false})
+    .pipe(clean())
+});
+
+gulp.task('build-html', () => {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.buildDest))
+});
+
+gulp.task('build-fonts', () => {
+  return gulp.src(paths.fonts.src)
+    .pipe(gulp.dest(paths.fonts.buildDest))
+});
+
+gulp.task('build-icons', () => {
+  return gulp.src(paths.icons.src)
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest(paths.icons.buildDest))
+});
+
+gulp.task('build-images', () => {
+  return gulp.src(paths.images.src)
+    .pipe(imageResize({
+      percentage: 8,
+      imageMagick: true
+    }))
+    .pipe(imagemin({progressive: true}))
+    .pipe(gulp.dest(paths.images.buildDest))
+});
+
+gulp.task('build-sass', () => {
+  return gulp.src(paths.scss.src)
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions']
+    }))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.scss.buildDest))
+});
+
+gulp.task('build-wicons', () => {
+  return gulp.src(paths.wicons.src)
+    .pipe(concat('wicons.css'))
+    .pipe(cleanCSS())
+    .pipe(gulp.dest(paths.wicons.buildDest))
+});
+
+gulp.task('build-shared-scripts', () => {
+  return gulp.src(paths.scripts.srcShared)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('shared.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.buildDest))
+});
+
+gulp.task('build-home-view', () => {
+  return gulp.src(paths.scripts.srcHomeView)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('index.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.buildDest))
+});
+
+gulp.task('build-sky-view', () => {
+  return gulp.src(paths.scripts.srcSkyView)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('sky.details.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.buildDest))
+});
+
+gulp.task('build-earth-view', () => {
+  return gulp.src(paths.scripts.srcEarthView)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('earth.details.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.buildDest))
+});
+
+gulp.task('build-space-view', () => {
+  return gulp.src(paths.scripts.srcSpaceView)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('space.details.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.buildDest))
+});
+
+gulp.task('build-loader', () => {
+  return gulp.src(paths.scripts.srcLoader)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(concat('loader.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.buildDest))
+});
+
+gulp.task('build-sw', () => {
+  return gulp.src(paths.scripts.sw.src)
+    .pipe(babel({
+      presets: ['@babel/env']
+    }))
+    .pipe(uglify())
+    .pipe(gulp.dest(paths.scripts.sw.buildDest))
+});
+
+gulp.task('build-manifest', () => {
+  return gulp.src(paths.scripts.sw.manifest)
+    .pipe(gulp.dest(paths.scripts.sw.buildDest))
+});
+
+gulp.task('build', buildTasks, () => {});
 
 gulp.task('default', () => {});
